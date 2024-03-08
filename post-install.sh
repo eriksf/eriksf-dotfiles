14b23389c635a7457c7144a10702deacf5a13e32
Erik Ferlanti <eferlanti@tacc.utexas.edu>
2024-03-08 12:55:23 -0600

    updated post-install

--- post-install.sh
+++ post-install.sh
@@ -1,10 +1,18 @@
 #!/bin/bash
 
+# check architecture
+ARCH=$(uname -m)
+if [[ "$ARCH" == "arm64" ]]; then
+    MYZSH=/opt/homebrew/bin/zsh
+else
+    MYZSH=/usr/local/bin/zsh
+fi
+
 # Change default shell
-if ! fgrep -q "/opt/homebrew/bin/zsh" /etc/shells; then
+if ! fgrep -q "$MYZSH" /etc/shells; then
   echo 'Changing default shell to zsh'
-  echo "/opt/homebrew/bin/zsh" | sudo tee -a /etc/shells
-  chsh -s /opt/homebrew/bin/zsh
+  echo "$MYZSH" | sudo tee -a /etc/shells
+  chsh -s $MYZSH
 else
   echo 'Already using zsh'
 fi
